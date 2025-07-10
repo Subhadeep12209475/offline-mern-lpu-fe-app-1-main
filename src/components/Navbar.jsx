8ccc43af2d1c57b2fabdd149a23ffc783c753071
Subhadeep Mukherjee <subhadeepmukherjee2019@gmail.com>
2025-07-10 09:45:05 +0530

    chnages color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 const Navbar = () => {
     return (
         <div className="py-4 px-6 flex justify-between bg-emerald-500">
-            <div className="font-bold text-emerald-600">Shopping App</div>
+            <div className="font-bold text-amber-300">Shopping App</div>
             <div className="flex gap-2">
                 <input className="b-1 py-1 px-2 rounded-md" />
                 <button className="b-1 py-1 px-2 rounded-md">Search</button>
